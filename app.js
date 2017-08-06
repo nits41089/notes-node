1e298a060085686703f542d84d3768866164f709
Nitin Thakur <nitin@crematrix.com>
2017-08-06 11:02:24 +0530

    uploading node tutorial in git

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Starting app.js...');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+console.log(_.uniq([2,2,3]));
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('Starting notes.js...');
+
+module.exports.addNote = () => {
+  console.log('Add Note.');
+  return 'New Note';
+};
